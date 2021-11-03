f91535307e8fbdb5434420874add8d2f84ebc627
Mauricio Salmoran <70659429+s11653565@users.noreply.github.com>
2021-11-03 08:42:26 -0400

    5B
    added scanner to output students 4 grades

--- .github/Student.java
+++ .github/Student.java
@@ -23,6 +23,42 @@ public class Student {
         this.gradePointAvg = GPA;
         this.studentName = studentName; 
     }
+
+
+
+
+
+
+
+
+    // More than one constructor is known as OVERLOADING.
+
+
+
+   
+   
+    // Creating Methods 
+
+    public static void calcGPA() {
+        int grade0;
+        int grade1;
+        int grade2;
+        int grade3;
+        double newGPA;
+
+        import java.util.scanner 
+        Scanner myScanner = new Scanner(System.in);
+        
+        System.out.println("What is the first grade");
+        grade0 = myScanner.nextInt();
+        System.out.println("What is the second grade");
+        grade1 = myScanner.nextInt();
+        System.out.println("What is the third grade");
+        grade2 = myScanner.nextInt();
+        System.out.println("What is the fourth grade");
+        grade3 = myScanner.nextInt();
+
+    }
     
 
 
