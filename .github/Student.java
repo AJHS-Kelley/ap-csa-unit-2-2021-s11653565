cfe551ed09f368906f5d71480a4c089df3147cb6
Mauricio Salmoran <70659429+s11653565@users.noreply.github.com>
2021-11-15 09:00:40 -0500

    5B v0.5
    Introduced bootlean operators

--- .github/Student.java
+++ .github/Student.java
@@ -1,6 +1,17 @@
 import java.until.Scanner;
 
-// Unit 2, Topic 2.1, Classes and objects, 11/05/21, Version 0.45
+// Unit 2, Topic 2.1, Classes and objects, 11/15/21, Version 0.5
+
+
+// Starting memory diagram 
+a = 4
+b = 3
+c = Ricer 
+
+// Ending memory diagram 
+a = a *8
+b = b /2
+c = Tuner 
 
 
 public class Student {
@@ -91,6 +102,34 @@ public class Student {
             System.out.print("This student has ");
             System.out.println(lunchPeriod);
         } else if (gradeLevel == 11) {
+            lunchPeriod = "Second Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        } else { 
+            lunchPeriod = "Outside Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        }
+
+
+    }
+
+    public static void assignLunchMultiple(int gradeLevel, double gradePointAvg) { 
+        String lunchPeriod; 
+        // AND, OR, NOT -- Boolean Operators
+        // && -- AND
+        // || -- OR
+        // ! -- NOT
+
+        if (gradeLevel == 9 || gradePoingAvg <= 1.5) {
+            lunchPeriod = "Third Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        } else if (gradeLevel == 10 && gradePointAvg > 2.0) { 
+            lunchPeriod = "First Lunch";
+            System.out.print("This student has ");
+            System.out.println(lunchPeriod);
+        } else if (gradeLevel == 11 && gradePointAvg != 0.0) {
             lunchPeriod = "Second Lunch";
             System.out.print("This student has ");
             System.out.println(lunchPeriod);
